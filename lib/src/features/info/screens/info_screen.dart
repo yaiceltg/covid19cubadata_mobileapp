b063c4ddd7c1ded2b8b9e4e17cade18695ef22a3
Yaicel Torres <yaiceltg@gmail.com>
2020-03-25 10:52:33 -0400

    Update info page

--- lib/src/features/info/screens/info_screen.dart
+++ lib/src/features/info/screens/info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoScreen extends StatefulWidget {
   InfoScreen({Key key}) : super(key: key);
@@ -9,6 +10,15 @@ class InfoScreen extends StatefulWidget {
 
 class _InfoScreenState extends State<InfoScreen> {
  
+
+  _openUrl(String url) async{
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +31,18 @@ class _InfoScreenState extends State<InfoScreen> {
           child: ListView(
             children: <Widget>[
               ListTile(
-                title: Text('La apliacación fue desarrollada usando los datos de la fuente https://covid19cubadata.github.io/'),
-                onTap: () {},
+                title: Text('La aplicación fue desarrollada usando los datos de la fuente:'),
+                subtitle: Text('https://covid19cubadata.github.io/'),
+                onTap: (){
+                  this._openUrl('https://covid19cubadata.github.io/');                
+                },
+              ),
+              ListTile(
+                title: Text('El código se encuentra publicado en:'),
+                subtitle: Text('https://github.com/yaiceltg/covid19cubadata_mobileapp'),
+                onTap: () {
+                  this._openUrl('https://github.com/yaiceltg/covid19cubadata_mobileapp');
+                },
               ),
             ],
           ),
